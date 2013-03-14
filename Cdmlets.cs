dd87e2152067726dbce6e3dfadac648c8346b295
Tim Rogers <tiroger@microsoft.com>
2013-03-14 15:22:08 -0700

    Updated copyright and license info

--- Cdmlets.cs
+++ Cdmlets.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright company="CoApp Project">
+//     Copyright (c) 2013 Tim Rogers and CoApp Contributors. 
+//     Contributors can be discovered using the 'git log' command.
+//     All rights reserved.
+// </copyright>
+// <license>
+//     The software is licensed under the Apache 2.0 License (the "License")
+//     You may not use the software except in compliance with the License. 
+// </license>
+//-----------------------------------------------------------------------
+
+
+using System;
 using System.Management.Automation;
 using ClrPlus.Powershell.Rest.Commands;
 
